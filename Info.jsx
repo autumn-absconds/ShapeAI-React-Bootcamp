b019e22e52fed22c4271c66465df734735666b05
Shubham Yadav <65112908+shuhzz@users.noreply.github.com>
2021-06-26 17:23:21 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript & React Bootcamp</h1>
+      <p>Start journey in Frontend Web dev. with Shauyra Sinha</p>
+    </div>
+  );
+}
+
+export default Info;
